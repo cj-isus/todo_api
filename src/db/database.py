8098dfde24a6001c14fedc105560b2c6e11598be
ivan <ivanrrodygin@yandex.ru>
2025-07-21 12:13:16 +0300

    Настройка модуля базы данных database.py

---
+++ src/db/database.py
@@ -0,0 +1,18 @@
+﻿# Импорт Tortoise ORM и настроек
+from tortoise import Tortoise
+from src.config.settings import settings
+
+# Инициализация базы данных
+async def init_db():
+    print(f"Initializing database with URL: {settings.DATABASE_URL}")
+    await Tortoise.init(
+        db_url=settings.DATABASE_URL,
+        modules={"models": ["src.db.models"]}
+    )
+    print("Generating schemas...")
+    await Tortoise.generate_schemas(safe=True)
+    print("Database initialized and schemas generated.")
+
+# Закрытие соединения с базой данных
+async def close_db():
+    await Tortoise.close_connections()
