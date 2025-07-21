638e2d4b4f24ad1ae135857c560d1123e4fb31f1
ivan <ivanrrodygin@yandex.ru>
2025-07-21 16:40:37 +0300

    Добавление модели Project и базового теста

---
+++ src/db/models/project.py
@@ -0,0 +1,16 @@
+﻿from tortoise import fields
+from tortoise.models import Model
+from tortoise.validators import MinLengthValidator
+
+class Project(Model):
+    id = fields.BigIntField(primary_key=True)
+    title = fields.CharField(max_length=255, validators=[MinLengthValidator(1)])
+    description = fields.TextField(null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(null=True)
+    user_id = fields.BigIntField()
+    user: fields.ForeignKeyRelation["src.db.models.User"] = fields.ForeignKeyField("models.User", related_name="projects")
+    tasks: fields.ManyToManyRelation["src.db.models.Task"] = fields.ManyToManyField("models.Task", through="task_project", related_name="projects")
+    class Meta:
+        table = "projects"
+        schema = "todo"
---
+++ tests/unit/test_project_model.py
@@ -0,0 +1,6 @@
+﻿from src.db.models.project import Project
+
+def test_project_model():
+    project = Project(title="Test Project", user_id=1)
+    assert project.title == "Test Project"
+    assert project.user_id == 1
